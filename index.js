9a3096119bea6859c05197f0822c13f32c1e8ff1
songyongchao <songych@163.com>
2018-07-09 15:40:10 +0800

    asdgasg

--- index.js
+++ index.js
@@ -3,12 +3,21 @@ const app = new Koa()
 const router = require('./router')
 const bodyParser = require('koa-bodyparser')
 const  serve = require("koa-static");
-app.use(serve(__dirname+ "/static/"));
 
 app.use(bodyParser())
+
+async function logger(ctx, next) {
+  const startDate = new Date();
+  console.log("ctx.url="+ctx.url);
+  next();
+  console.log(`method: ${ctx.method} code: ${ctx.status} time:${new Date() -startDate}ms`);
+}
+app.use(logger);
+app.use(serve(__dirname+ "/static/"));
 
 app
   .use(router.routes())
   .use(router.allowedMethods())
+
 
 app.listen(3333)
